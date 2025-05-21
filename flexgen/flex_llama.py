5b629a1d26852865524eb4ca13c4e43974d484d2
Xuchuan Luo <river861@qq.com>
2025-05-22 01:42:23 +0800

    test

--- flexgen/flex_llama.py
+++ flexgen/flex_llama.py
@@ -839,7 +839,7 @@ class LlamaLM:
         self,
         inputs,
         max_new_tokens: int=32,
-        do_sample: bool=True,
+        do_sample: bool=False,
         temperature: float=0.6,
         stop: Optional[int] = None,
         debug_mode: Optional[str] = None,
